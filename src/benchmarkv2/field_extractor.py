c22fd824f27dbbe86bc04186e9aa24a648873723
Shlok Natarajan <shlok.natarajan@gmail.com>
2026-01-16 17:19:07 -0800

    chore: ruff

--- src/benchmarkv2/field_extractor.py
+++ src/benchmarkv2/field_extractor.py
@@ -1,10 +1,9 @@
 """
 Given a proposed annotation, extract the fields that are relevant to the benchmark.
-Should be able to take an argument of variant, drug, or phenotype and get all the values from those fields 
+Should be able to take an argument of variant, drug, or phenotype and get all the values from those fields
 across annotations from the json file
 function: (proposed_annotation: str | Path to json file (ex. data/proposed_annotations/PMC384715.json), dict: {pmcid [str]: all the fields list[str]})
 """
-
 
 import json
 from enum import Enum
@@ -13,14 +12,17 @@ from pydantic import BaseModel
 
 class FieldTypes(Enum):
     """An enum representing the three types of fields we care about: drug, variant, or phenotype."""
+
     DRUG = "drug"
     VARIANT = "variant"
     PHENOTYPE = "phenotype"
+
 
 class SingleArticleFields(BaseModel):
     pmcid: str
     type: FieldTypes
     fields: list[str]
+
 
 def null_to_empty(value: str | None) -> str:
     """Converts null string values to empty strings"""
@@ -43,7 +45,7 @@ def fields_from_file(file_path: str, field_type: str) -> SingleArticleFields:
     field_map = {
         "drug": "Drug(s)",
         "variant": "Variant/Haplotypes",
-        "phenotype": "Phenotype"
+        "phenotype": "Phenotype",
     }
 
     field_key = field_map[field_type]
@@ -61,6 +63,7 @@ def fields_from_file(file_path: str, field_type: str) -> SingleArticleFields:
             fields.append(value)
 
     return SingleArticleFields(pmcid=pmcid, type=FieldTypes(field_type), fields=fields)
+
 
 if __name__ == "__main__":
     print(fields_from_file("data/proposed_annotations/PMC384715.json", "drug"))
